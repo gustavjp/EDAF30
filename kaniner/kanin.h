6c4a3493f3895e4d9b56b54ffb2cd79b6c7ad721
Gustav Jansson Ekstrand <gustav.jp@live.se>
2015-01-07 04:03:03 +0100

    Kaniner done

--- kaniner/kanin.h
+++ kaniner/kanin.h
@@ -4,29 +4,26 @@
 using namespace std;
 
 class Kanin {
-
 public:
     Kanin(string n = "anonymous coward") : name(n) {
-        agility = rand()%100 + 1;
         distance = 0;
     };
-    int getAgility() { return agility; };
+    int getDistance() { return distance; };
     string getName() { return name; };
     void jump() {
-        action = rand()%100 + 1;
-        if(action <= 40) {
-            distance += rand()%20 + 20;
-        } else if(action > 40 && action <= 60) {
+        action = rand()%101; //0-100
+        if(action < 20) { //20% chance of jumping backwards
             distance -= rand()%20 + 20;
-        } else {
+        } else if(action >= 20 && action <= 60) { //40% chance of standing still
             //gör inget
+        } else if(action > 60) { //40% chance of jumping forwards
+            distance += rand()%20 + 20;
         }
     };
 private:
-    int agility;
-    string name;
-    int distance;
-    int action;
+    string name; //the name of the rabbit
+    int distance; //the distance the rabbit has jumped from its initial position
+    int action; //the action to be taken (backwards, nothing, forwards)
 };
 
 #endif
--- kaniner/main.cpp
+++ kaniner/main.cpp
@@ -1,49 +1,105 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <climits>
+#include <limits>
 #include "kanin.h"
-#include "quicksort.h"
 
 using namespace std;
 
-vector<string> createNameVector(int kaniner);
+int getInput();
+vector<Kanin> raceSetup(int kaniner);
+void race(vector<Kanin> kaninVector);
+vector<string> createNameVector();
 string getRandName(vector<string> &nameVector);
+vector<Kanin> sortByDistance(vector<Kanin> unsortedVector);
 int main()
 {
     srand(time(0));
-    int kaniner = 10;
-    if(kaniner > 10) {
-        kaniner = 10;
+    int kaniner = 0;
+    cout << "============" << endl;
+    cout << "Rabbit Race!" << endl;
+    cout << "============" << endl << endl;
+    while(true) {
+        cout << "How many rabbits (1 to 23) shall compete?";
+        kaniner = getInput();
+        if(kaniner < 24 && kaniner > 0) {
+            cout << kaniner << " rabbits are to compete." << endl << "Let's start the race!" << endl;
+            race(raceSetup(kaniner));
+            cout << "Input 1 to play again or any other character to exit.";
+            if(getInput() != 1) {
+                break;
+            }
+        } else {
+            cout << "Rabbits out of range. Please chose 1 to 23 rabbits." << endl;
+        }
     }
-    vector<string> nameVector (createNameVector(kaniner));
-    vector<Kanin> kaninVector;
-    Kanin kaninArray[10];
+    return 0;
+}
 
+/**
+  * Handles input.
+  */
+int getInput() {
+    int input;
+    cout << endl << "$ ";
+    if(!(cin >> input)) { //checks that the input is an integer
+        cout << "Please only use integers as input." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return 0; //returns 0 if input is not an integer
+        }
+    return input;
+}
+
+/**
+  * Creates a vector of rabbits.
+  * Setups the race.
+  */
+vector<Kanin> raceSetup(int kaniner) {
+    vector<string> nameVector (createNameVector());
+    vector<Kanin> kaninVector;
     for(int i = 0; i < kaniner; i++) {
         Kanin kanin = {getRandName(nameVector)};
         kaninVector.insert(kaninVector.begin()+i, kanin);
-        kaninArray[i] = kanin;
     }
-    for(int i = 0; i < kaniner; i++) {
-        cout << "Rabbit " << kaninArray[i].getName() << " has agility " << kaninArray[i].getAgility() << endl;
-    }
-    cout << "Sorting..." << endl;
-    quicksort(kaninArray, 0, 9);
-    for(int i = 0; i < kaniner; i++) {
-        cout << "Rabbit " << kaninArray[i].getName() << " has agility " << kaninArray[i].getAgility() << endl;
-    }
+    return kaninVector;
+}
 
-    return 0;
+/**
+  * This is the race.
+  */
+void race(vector<Kanin> kaninVector) {
+    int track = 1000; //1000 cm = 10 m
+    bool win = false;
+    while(!win) { //loops until a rabbit has won
+        for(int i = 0; i < (int) kaninVector.size(); i++) {
+            kaninVector.at(i).jump();
+            if(kaninVector.at(i).getDistance() >= track) {
+                win = true;
+                cout << endl << "We have a winner!" << endl;
+                cout << kaninVector.at(i).getName() << " won!" << endl;
+                break;
+            }
+        }
+    }
+    kaninVector = sortByDistance(kaninVector);
+    cout << "Here are the results!" << endl;
+    for(int i = 0; i < (int) kaninVector.size(); i++) {
+        cout << kaninVector.at(i).getName() << ": " << kaninVector.at(i).getDistance() << endl;
+    }
 }
 
 /**
   * Creates a vector of strings with iconic names of Star Trek: The Next Generation characters.
   */
-vector<string> createNameVector(int kaniner) {
-    string names[10] = {"Jean-Luc Picard", "William Riker", "Beverly Crusher", "Deanna Troi",
-    "Geordi La Forge", "Data", "Worf", "Wesley Crusher", "Guinan", "Tasha Yar"};
+vector<string> createNameVector() {
+    string names[23] = {"Jean-Luc Picard", "William Riker", "Beverly Crusher", "Deanna Troi",
+    "Geordi La Forge", "Data", "Worf", "Wesley Crusher", "Guinan", "Tasha Yar", "Miles O'Brien",
+    "Katherine Pulaski", "Reginald \"Broccoli\" Barclay", "Alyssa Ogawa", "Mr. Homm", "Gowron",
+    "Alexander Rozhenko", "Q", "Lore", "The Traveler", "Hugh of Borg", "Locutus", "Zefram Cochrane"};
     vector<string> nameVector;
-    nameVector.assign(names, names+kaniner);
+    nameVector.assign(names, names+23);
     return nameVector;
 }
 
@@ -55,4 +111,27 @@ string getRandName(vector<string> &nameVector) {
     string name = nameVector.at(i);
     nameVector.erase(nameVector.begin()+i);
     return name;
+}
+
+/**
+  * A simple selection sorting mechanism.
+  * Sorts a vector of rabbits in descending order of distance.
+  */
+vector<Kanin> sortByDistance(vector<Kanin> unsortedVector) {
+    vector<Kanin> sortedVector;
+    int maximum = INT_MIN;
+    int index = 0;
+    while(!unsortedVector.empty()) {
+        for(int i = 0; i < (int) unsortedVector.size(); i++) {
+            if(unsortedVector.at(i).getDistance() > maximum) {
+                maximum = unsortedVector.at(i).getDistance();
+                index = i;
+            }
+        }
+        sortedVector.push_back(unsortedVector.at(index));
+        unsortedVector.erase(unsortedVector.begin()+index);
+        maximum = INT_MIN;
+        index = 0;
+    }
+    return sortedVector;
 }
